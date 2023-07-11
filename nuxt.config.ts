5b330565dec38baad4408dc63d4a9c25bead987e
Agboola Temitope <85450650+kaiser54@users.noreply.github.com>
2023-07-11 21:29:28 +0100

    testing nuxt 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      meta: [
+        // <meta name="viewport" content="width=device-width, initial-scale=1">
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      script: [
+        // <script src="https://myawesome-lib.js"></script>
+        { src: 'https://awesome-lib.js' }
+      ],
+      link: [
+        // <link rel="stylesheet" href="https://myawesome-lib.css">
+        { rel: 'stylesheet', href: 'https://awesome-lib.css' }
+      ],
+      // please note that this is an area that is likely to change
+      style: [
+        // <style type="text/css">:root { color: red }</style>
+        // { children: ':root { color: red }', type: 'text/css' }
+      ],
+      noscript: [
+        // <noscript>JavaScript is required</noscript>
+        { children: 'JavaScript is required' }
+      ]
+    }
+  },
+  pages: true,
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    "@/assets/css/reset.css",
+    "@/assets/css/base.css",
+    "@/assets/css/main.css",
+    "animate.css/animate.min.css",
+  ],  
 })
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>TESTING NUXT 3</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
